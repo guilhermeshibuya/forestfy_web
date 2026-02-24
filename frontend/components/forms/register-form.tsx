fce1001ad89a65e87867d0fc6c80cfe2d50cd69e
Guilherme Shibuya <gyudishibuya@gmail.com>
2026-02-24 20:14:40 -0300

    feat: add login and register form component

--- frontend/components/forms/register-form.tsx
+++ frontend/components/forms/register-form.tsx
@@ -1,36 +1,37 @@
 'use client'
 
-import { LoginFormData, loginSchema } from '@/schemas/login-schema'
-import { login } from '@/services/auth-service'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { Field, FieldGroup, FieldLabel } from '../ui/field'
 import { Input } from '../ui/input'
 import { Button } from '../ui/button'
 import { useRouter } from 'next/navigation'
+import { RegisterFormData, registerSchema } from '@/schemas/register-schema'
 import { FORM_MESSAGES } from '@/constants/form-messages'
+import { register as registerService } from '@/services/auth-service'
 
-export function LoginForm() {
+export function RegisterForm() {
   const router = useRouter()
 
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
-  } = useForm<LoginFormData>({
-    resolver: zodResolver(loginSchema),
+  } = useForm<RegisterFormData>({
+    resolver: zodResolver(registerSchema),
     defaultValues: {
+      name: '',
       email: '',
       password: '',
     },
   })
 
-  async function onSubmit(data: LoginFormData) {
+  async function onSubmit(data: RegisterFormData) {
     try {
-      await login(data)
+      await registerService(data)
       router.push('/dashboard')
     } catch {
-      alert('Login failed')
+      alert('Erro ao registrar usuário')
     }
   }
 
@@ -38,15 +39,24 @@ export function LoginForm() {
     <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
       <FieldGroup>
         <Field>
+          <FieldLabel htmlFor="name">{FORM_MESSAGES.NAME_LABEL}</FieldLabel>
+          <Input
+            type="text"
+            placeholder={FORM_MESSAGES.NAME_PLACEHOLDER}
+            {...register('name')}
+          />
+          {errors.name && (
+            <span className="text-red-500">{errors.name.message}</span>
+          )}
+        </Field>
+
+        <Field>
           <FieldLabel htmlFor="email">{FORM_MESSAGES.EMAIL_LABEL}</FieldLabel>
           <Input
             type="email"
             placeholder={FORM_MESSAGES.EMAIL_PLACEHOLDER}
             {...register('email')}
           />
-          {errors.email && (
-            <span className="text-red-500">{errors.email.message}</span>
-          )}
         </Field>
 
         <Field>
@@ -65,8 +75,8 @@ export function LoginForm() {
 
         <Button type="submit" className="w-full" disabled={isSubmitting}>
           {isSubmitting
-            ? FORM_MESSAGES.LOGIN_SUBMITTING
-            : FORM_MESSAGES.LOGIN_BUTTON}
+            ? FORM_MESSAGES.REGISTER_SUBMITTING
+            : FORM_MESSAGES.REGISTER_BUTTON}
         </Button>
       </FieldGroup>
     </form>
