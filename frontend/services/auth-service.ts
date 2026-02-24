e5485ab6e85dcaa9ddb13d1684d1d976fa6cae48
Guilherme Shibuya <gyudishibuya@gmail.com>
2026-02-24 20:16:37 -0300

    add login and register service

--- frontend/services/auth-service.ts
+++ frontend/services/auth-service.ts
@@ -1,14 +1,31 @@
 import { LoginFormData } from '@/schemas/login-schema'
-import { api } from '@/utils/api'
+import { RegisterFormData } from '@/schemas/register-schema'
+import { api, loginEndpoint, registerEndpoint } from '@/utils/api'
 
 export async function login(data: LoginFormData) {
   const formData = new FormData()
 
-  formData.append('username', data.username)
+  formData.append('username', data.email)
   formData.append('password', data.password)
 
-  return await api('http://localhost:8000/api/v1/auth/login', {
+  return await api(loginEndpoint(), {
     method: 'POST',
     body: formData,
+  })
+}
+
+export async function register(data: RegisterFormData) {
+  const newUser = {
+    full_name: data.name,
+    email: data.email,
+    password: data.password,
+  }
+
+  return await api(registerEndpoint(), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify(newUser),
   })
 }
